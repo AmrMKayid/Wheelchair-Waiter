993d990e20818fbbeb6ef5b272788785e1a07e7a
Amr M. Kayid <amrkayid2027@gmail.com>
2018-07-26 10:31:33 +0200

    Organize the Project

---
+++ Assets/_Scripts/PlateController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class PlateController : MonoBehaviour {
+    private float nextActionTime = 0.0f;
+    public float period = 2.0f;
+    public GameObject gameObject;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextActionTime)
+        {
+            Instantiate(gameObject, new Vector3(0.218f, 1.231f, 2.629f), Quaternion.identity);
+            nextActionTime += period;
+            // execute block of code here
+        }
+    }
+    
+
+}
---
+++ Assets/_Scripts/plate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using VRTK;
+public class plate : MonoBehaviour {
+
+    float direction;
+    public float maxLimit, lowerLimit, xChange;
+    private bool first = true, finished=false;
+    void start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(!finished)
+        {
+            if (first)
+        {
+            GetComponent<VRTK_InteractableObject>().InteractableObjectGrabbed += new InteractableObjectEventHandler(ObjectGrabbed);
+                maxLimit = transform.position.y + 20;// you set this to your desired height
+            lowerLimit = transform.position.y;//this can be whatever you want as well
+            direction = 0.05f;// you can put this as any number you want, the larger the number the more it will look like teleporting, the smaller the number the more smooth but the less distance it will travel per frame/time
+            float rand = Random.Range(0f, 0.9f);
+            Debug.Log(rand);
+            Console.WriteLine("Hello");
+            xChange = rand;
+            first = false;
+        }
+        transform.position = transform.position + new Vector3(Time.fixedDeltaTime*xChange, direction, -Time.fixedDeltaTime * 0.7f);
+        //Debug.Log(xChange);
+        if (transform.position.y <= maxLimit)
+        {
+            //Debug.Log(Time.fixedDeltaTime);
+            direction = Time.fixedDeltaTime * 0.001f;
+        }
+        }
+    }
+    private void ObjectGrabbed(object sender, InteractableObjectEventArgs e)
+
+    {
+
+        finished = true;
+
+    }
+}
